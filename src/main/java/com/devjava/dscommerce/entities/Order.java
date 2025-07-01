468b6d5f428349038eeda024f510b40b6df9f950
Sandro Henrique Vaz Ribovski <sandro.ribovski@liggatelecom.com.br>
2025-07-01 20:27:54 -0300

    add Payment

--- src/main/java/com/devjava/dscommerce/entities/Order.java
+++ src/main/java/com/devjava/dscommerce/entities/Order.java
@@ -20,4 +20,56 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order() {
+    }
+    public Order(Long id, Instant moment, String status, User client, Payment payment) {
+        this.id = id;
+        this.moment = moment;
+        Status = status;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 }
